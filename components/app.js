2a94d05dcda47c7217ccd0243db31f8c09fe3082
Ole <ole.richter91@gmail.com>
2020-04-27 11:27:21 +0200

    fix safari overflow

--- components/app.js
+++ components/app.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react"
 
 import { AppContext, Header, VocabularyList, Navigation, Pending } from "."
+import styles from "../styles/app.module.scss"
 
 const App = () => {
   const { loadingCountry, extendedNavigation } = useContext(AppContext)
@@ -8,7 +9,7 @@ const App = () => {
   if (loadingCountry) return <Pending text="Getting your current location..." />
 
   return (
-    <div>
+    <div className={styles.app}>
       <Header extended={extendedNavigation} />
       <Navigation />
       <VocabularyList />
